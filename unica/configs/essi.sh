174a7c7b444b32dba3963efa13e6a26620b9b4ed
techyguyperplexable <hlcj1234567@gmail.com>
2025-08-17 22:50:18 +0100

    unica: Add tab s9 FE base

--- unica/configs/essi.sh
+++ unica/configs/essi.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2025 Salvo Giangreco
+# Copyright (C) 2023 Salvo Giangreco
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,40 +15,52 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# ExtremeROM configuration file for Exynos devices (essi)
+# UN1CA configuration file
+ROM_VERSION="2.0.0"
+ROM_VERSION+="-$(git rev-parse --short HEAD)"
+ROM_CODENAME="Nexus"
 
-# Galaxy S24 FE (One UI 7)
-SOURCE_CODENAME="r12s"
-SOURCE_FIRMWARE="SM-S721B/EUX/351273090276500"
-SOURCE_EXTRA_FIRMWARES=()
-SOURCE_API_LEVEL=35
-SOURCE_PRODUCT_FIRST_API_LEVEL=34
-SOURCE_VNDK_VERSION="34"
-SOURCE_HAS_SYSTEM_EXT=false
-SOURCE_SUPER_GROUP_NAME="group_basic"
+# Source ROM firmware
+case "$TARGET_SINGLE_SYSTEM_IMAGE" in
+    "essi")
+        # Galaxy Tab S9 FE 5G
+        SOURCE_CODENAME="gts9fe"
+        SOURCE_FIRMWARE="SM-X516B/EUX/354136923537868"
+        SOURCE_EXTRA_FIRMWARES=("")
+        SOURCE_API_LEVEL=35
+        SOURCE_PRODUCT_FIRST_API_LEVEL=35
+        SOURCE_VNDK_VERSION="33"
+        SOURCE_HAS_SYSTEM_EXT=true
+        SOURCE_SUPER_GROUP_NAME="samsung_dynamic_partitions"
+        
+        # SEC Product Feature
+        SOURCE_AUTO_BRIGHTNESS_TYPE="5"
+        SOURCE_DVFS_CONFIG_NAME="dvfs_policy_exynos1380"
+        SOURCE_NFC_CHIP_VENDOR="none"
+        SOURCE_FP_SENSOR_CONFIG="none"
+        SOURCE_HAS_MASS_CAMERA_APP=false
+        SOURCE_HAS_QHD_DISPLAY=true
+        SOURCE_HFR_MODE="3"
+        SOURCE_HFR_SUPPORTED_REFRESH_RATE="60,90"
+        SOURCE_HFR_DEFAULT_REFRESH_RATE="90"
+        SOURCE_DISPLAY_CUTOUT_TYPE="none"
+        SOURCE_IS_ESIM_SUPPORTED=false
+        SOURCE_HAS_HW_MDNIE=true
+        SOURCE_MDNIE_SUPPORTED_MODES="65303"
+        SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+        SOURCE_SUPPORT_WIFI_7=false
+        SOURCE_SUPPORT_HOTSPOT_DUALAP=true
+        SOURCE_SUPPORT_HOTSPOT_WPA3=true
+        SOURCE_SUPPORT_HOTSPOT_6GHZ=false
+        SOURCE_SUPPORT_HOTSPOT_WIFI_6=true
+        SOURCE_SUPPORT_HOTSPOT_ENHANCED_OPEN=true
+        SOURCE_AUDIO_SUPPORT_ACH_RINGTONE=true
+        SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION=true
+        ;;
+    *)
+        echo "\"$TARGET_SINGLE_SYSTEM_IMAGE\" is not a valid system image."
+        return 1
+        ;;
+esac
 
-# SEC Product Feature
-SOURCE_AUTO_BRIGHTNESS_TYPE="5"
-SOURCE_DVFS_CONFIG_NAME="dvfs_policy_default"
-SOURCE_NFC_CHIP_VENDOR="SLSI"
-SOURCE_FP_SENSOR_CONFIG="google_touch_display_optical,settings=3,aod_transition_animation"
-SOURCE_HAS_MASS_CAMERA_APP=true
-SOURCE_HAS_QHD_DISPLAY=false
-SOURCE_HFR_MODE="2"
-SOURCE_HFR_SUPPORTED_REFRESH_RATE="60,120"
-SOURCE_HFR_DEFAULT_REFRESH_RATE="120"
-SOURCE_DISPLAY_CUTOUT_TYPE="center"
-SOURCE_HFR_SEAMLESS_BRT="89,91"
-SOURCE_HFR_SEAMLESS_LUX="260,2500"
-SOURCE_IS_ESIM_SUPPORTED=true
-SOURCE_HAS_HW_MDNIE=false
-SOURCE_MDNIE_SUPPORTED_MODES="37905"
-SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
-SOURCE_SUPPORT_WIFI_7=false
-SOURCE_SUPPORT_HOTSPOT_DUALAP=true
-SOURCE_SUPPORT_HOTSPOT_WPA3=true
-SOURCE_SUPPORT_HOTSPOT_6GHZ=true
-SOURCE_SUPPORT_HOTSPOT_WIFI_6=true
-SOURCE_SUPPORT_HOTSPOT_ENHANCED_OPEN=true
-SOURCE_AUDIO_SUPPORT_ACH_RINGTONE=false
-SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION=true
+return 0
